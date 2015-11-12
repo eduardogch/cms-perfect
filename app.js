c0c5c0530da3ccaa412a80405048ffaa4ce14ce4
Eduardo Gonzalez <eduardo.gch@gmail.com>
2015-11-12 18:53:31 -0500

    Move Angular files to App folder for better understanding and create node modules for actionhero, thinky and express

--- app.js
+++ app.js
@@ -265,5 +265,9 @@ app.listen(app.get('port'), function() {
   console.log('Express server listening on port %d in %s mode', app.get('port'), app.get('env'));
 });
 
+/**
+ * Nodejs Modules, RethinkDB, Actionhero and Express
+ */
 exports.thinky = thinky;
+exports.actionhero = actionhero;
 exports.app = app;
